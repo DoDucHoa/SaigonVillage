1156aae39b820a9cfcaf4cc8f16b986ad3feafa3
Do Duc Hoa <hhoa0978@gmail.com>
2024-10-09 23:08:36 +0200

    Refactor Chef Main, News component and remove unused code

--- src/components/Chef/Main.js
+++ src/components/Chef/Main.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import Chefsection from '../Menchefs/Chefsection'
-import Quotesection from '../Menchefs/Quotesection'
-import Teem from '../Menhome/Teem'
-import Bredcrumb from '../Bredcrumb/Main'
-import Img from '../../assets/images/background/banner-image-5.jpg'
+import React from "react";
+import Chefsection from "../Menchefs/Chefsection";
+import Quotesection from "../Menchefs/Quotesection";
+import ChefSection from "../Menhome/ChefSection";
+import Bredcrumb from "../Bredcrumb/Main";
+import Img from "../../assets/images/background/banner-image-5.jpg";
 
 function Main() {
   return (
-
     <>
       <Bredcrumb subtitle="Meet Our Chef" title="OUR TEAM" Img={Img} />
       <Chefsection />
       <Quotesection />
-      <Teem />
+      <ChefSection />
     </>
-
-  )
+  );
 }
 
-export default Main
+export default Main;
