9fb7e25b7699e3edcb73a85cf420e348e7dc2174
Do Duc Hoa <hhoa0978@gmail.com>
2024-11-19 23:28:05 +0100

    Adjust container styles in EmbeddedCanvaDesign for mobile responsiveness

--- src/components/Menu/EmbeddedCanvaDesign.js
+++ src/components/Menu/EmbeddedCanvaDesign.js
@@ -1,15 +1,19 @@
+import useDeviceType from "../../hooks/useDeviceType";
+
 const EmbeddedCanvaDesign = ({ embedLink }) => {
+  const isMobile = useDeviceType() === "mobile";
+
   const containerStyle = {
     position: "relative",
     width: "100%",
     height: 0,
-    paddingTop: "141.4286%",
+    paddingTop: isMobile ? "141.4286%" : "80%",
     paddingBottom: 0,
     boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
     marginTop: "1.6em",
     marginBottom: "0.9em",
     overflow: "hidden",
-    borderRadius: "8px",
+    borderRadius: "16px",
     willChange: "transform",
   };
 
