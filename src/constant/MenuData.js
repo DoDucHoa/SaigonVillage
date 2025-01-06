aa9b351986f270cf9c03ab8ae4cd335169eb669d
Do Duc Hoa <hhoa0978@gmail.com>
2025-01-06 17:31:42 +0100

    Remove Christmas tab entry from MenuData

--- src/constant/MenuData.js
+++ src/constant/MenuData.js
@@ -1,10 +1,5 @@
 export const tabLabels = [
   {
-    id: "christmas",
-    text: "menuCategories.christmas",
-    link: "https://www.canva.com/design/DAGYyH-066s/b4rpYexuu7jVoe5gwaK0OA/view?embed",
-  },
-  {
     id: "dinner",
     text: "menuCategories.dinner",
     link: "https://www.canva.com/design/DAGZ1yI4yXk/6cMKgct94RxfSOjjEQfAEw/view?embed",
