74c1b49a953204cd9d59cf917f092579e47b3668
Do Duc Hoa <hhoa0978@gmail.com>
2025-05-09 20:02:54 +0200

    Update preloader styles and implement custom loading component; modify index.js to use Scrool as fallback in Suspense

--- src/components/Menhome/Scrool.js
+++ src/components/Menhome/Scrool.js
@@ -3,30 +3,54 @@ import React from 'react'
 function Scrool() {
     return (
         <>
-            <div className="loader-wrap">
+            <div className="loader-wrap" style={{ backgroundColor: 'rgb(23, 24, 25)' }}>
                 <div className="preloader">
                     <div className="preloader-close">x</div>
                     <div id="handle-preloader" className="handle-preloader">
                         <div className="animation-preloader">
                             <div className="spinner"></div>
                             <div className="txt-loading">
-                                <span data-text-preloader="D" className="letters-loading">
-                                    D
+                                <span data-text-preloader="S" className="letters-loading">
+                                    S
                                 </span>
-                                <span data-text-preloader="E" className="letters-loading">
-                                    E
+                                <span data-text-preloader="A" className="letters-loading">
+                                    A
+                                </span>
+                                <span data-text-preloader="I" className="letters-loading">
+                                    I
+                                </span>
+                                <span data-text-preloader="G" className="letters-loading">
+                                    G
+                                </span>
+                                <span data-text-preloader="O" className="letters-loading">
+                                    O
+                                </span>
+                                <span data-text-preloader="N" className="letters-loading">
+                                    N
+                                </span>
+                                <span data-text-preloader=" " className="letters-loading">
+                                    &nbsp;
+                                </span>
+                                <span data-text-preloader="V" className="letters-loading">
+                                    V
+                                </span>
+                                <span data-text-preloader="I" className="letters-loading">
+                                    I
                                 </span>
                                 <span data-text-preloader="L" className="letters-loading">
                                     L
                                 </span>
-                                <span data-text-preloader="I" className="letters-loading">
-                                    I
+                                <span data-text-preloader="L" className="letters-loading">
+                                    L
                                 </span>
-                                <span data-text-preloader="C" className="letters-loading">
-                                    C
+                                <span data-text-preloader="A" className="letters-loading">
+                                    A
                                 </span>
-                                <span data-text-preloader="I" className="letters-loading">
-                                    I
+                                <span data-text-preloader="G" className="letters-loading">
+                                    G
+                                </span>
+                                <span data-text-preloader="E" className="letters-loading">
+                                    E
                                 </span>
                             </div>
                         </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Scrool from "./components/Menhome/Scrool";
 
 import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Suspense fallback="loading">
+    <Suspense fallback={<Scrool />}>
       <App />
     </Suspense>
   </React.StrictMode>
