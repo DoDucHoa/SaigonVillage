9a67b39adfb0e5b2dd0e2ba44cafaefc9b61374f
Do Duc Hoa <hhoa0978@gmail.com>
2025-06-26 15:34:18 +0200

    Change Canva link

--- src/constant/MenuData.js
+++ src/constant/MenuData.js
@@ -2,12 +2,12 @@ export const tabLabels = [
   {
     id: "dinner",
     text: "menuCategories.dinner",
-    link: "https://www.canva.com/design/DAGZ1yI4yXk/6cMKgct94RxfSOjjEQfAEw/view?embed",
+    link: "https://www.canva.com/design/DAGrKPt4l-I/H5KugKlHDRg3cgos4Bwz7A/view?embed",
   },
   {
     id: "lunch",
     text: "menuCategories.lunch",
-    link: "https://www.canva.com/design/DAGaa0jf38I/kbz9tsHp4QacmDc9to2m5Q/view?embed",
+    link: "https://www.canva.com/design/DAGrJ_MXZh0/bsPQli84vPhZ5S1hfJUIgA/view?embed",
   },
 ];
 
