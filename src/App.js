d171a53a336f55162a84710d252cebc380b54569
Do Duc Hoa <hhoa0978@gmail.com>
2025-07-30 21:44:39 +0200

    Add RestaurantModal component with auto-popup feature and customizable content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Routing from "./routes";
 import ErrorBoundary from "./components/common/ErrorBoundary";
 import "./assets/css/style.css";
 import "./assets/css/responsive.css";
+import RestaurantModal from "./components/Modal/RestaurantModal";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Router>
         <Routing />
       </Router>
+      <RestaurantModal />
     </ErrorBoundary>
   );
 }
